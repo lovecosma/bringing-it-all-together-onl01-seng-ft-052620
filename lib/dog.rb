15ec2bf11aaeded1a74aa7188a59837555ae5a67
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-26 18:57:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -100,8 +100,9 @@ def self.find_or_create_by(name:, breed:, id: nil)
   FROM dogs
   WHERE dogs.name = ?, dogs.breed = ?
   SQL
-binding.pry
   row = DB[:conn].execute(sql, this_name, this_breed).flatten
+  binding.pry
+
   this_id = row[0]
   if this_id == nil
     this_dog = Dog.create(name: this_name, breed: this_breed)
