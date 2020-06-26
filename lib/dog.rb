ec6843d430c434a2f18d4fff41911788d008cc18
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-26 18:55:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -96,7 +96,7 @@ def self.find_or_create_by(name:, breed:, id: nil)
   this_name = name
   this_breed = breed
   sql = <<-SQL
-  SELECT dogs.id
+  SELECT *
   FROM dogs
   WHERE dogs.name = ?, dogs.breed = ?
   SQL
