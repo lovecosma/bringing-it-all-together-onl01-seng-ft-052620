52bf65ea02935a5b06d3b7d6a6508f6c29ca8288
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-26 18:49:03 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -100,7 +100,7 @@ def self.find_or_create_by(name:, breed:, id: nil)
   WHERE dogs.name = ?, dogs.breed = ?
   SQL
 
-  DB[:conn].execute(sql, this_name, this_breed).flatten
+  row = DB[:conn].execute(sql, this_name, this_breed).flatten
 
   # this_dog = nil
 
