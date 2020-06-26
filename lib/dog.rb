20c4243d34ee7ed8f49f0a482d0f79e1fa2809c8
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-26 18:49:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -101,7 +101,7 @@ def self.find_or_create_by(name:, breed:, id: nil)
   SQL
 
   row = DB[:conn].execute(sql, this_name, this_breed).flatten
-  this_id = id
+  this_id = row[0]
   #
   # if this_id == nil
   #   this_dog = Dog.create(name: this_name, breed: this_breed)
